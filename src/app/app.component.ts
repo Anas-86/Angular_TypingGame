4503ed8503a9e04c1f5477f8e40e78285c1e6718
Anas-86 <anas.kala@hotmail.com>
2023-09-09 19:39:00 +0200

    game of typing a text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'game';
+  randomText = '';
+  enteredText = '';
+  ngOnInit() {
+    this.randomText = lorem.sentence();
+  }
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
